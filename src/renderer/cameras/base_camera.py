278694081b3279b2b61559c456e957bdb12d3ecb
Seungwoo Yoo <dreamy1534@gmail.com>
2023-07-14 00:20:38 +0900

    Remove legacy code

--- src/renderer/cameras/base_camera.py
+++ src/renderer/cameras/base_camera.py
@@ -25,25 +25,3 @@ class Camera(ABC):
         """
         Builds the OpenGL projection matrix for the current camera.
         """
-
-    @abstractmethod
-    def generate_screen_coords(self) -> Int[Tensor, "num_pixel 2"]:
-        """
-        Generates screen coordinates corresponding to image pixels.
-        """
-
-    @abstractmethod
-    def generate_ray_directions(
-        self,
-        screen_coords: Int[Tensor, "num_pixel 2"],
-    ) -> Float[Tensor, "num_pixel 3"]:
-        """
-        Computes ray directions for the current camera.
-        The direction vectors are represented in the camera frame.
-        """
-
-    # @abstractmethod
-    # def generate_rays(self) -> RayBundle:
-    #     """
-    #     Generates rays for the current camera.
-    #     """
